41afdcb418f21ca80aa9987ae596cf041b27eae8
adahn <adahn@desktop-linux>
2012-07-08 21:45:34 +0200

    Dependencies for md3 and shaders

--- tools/bsp.py
+++ tools/bsp.py
@@ -1,4 +1,5 @@
 # checks for dependencies (textures, shaders, models) in given bsp file
+# TODO: sounds
 
 import struct
 
@@ -23,21 +24,24 @@ LUMPS_NUMBERS = {
 }
 
 def get_bsp_deps(bsp_path):
-    with open(bsp_path, 'rb') as bsp:
-        check_header(bsp)
-        lumps = parse_dir(bsp)
-        return (parse_entities(bsp, lumps[LUMPS_NUMBERS['Entities']]),
-                parse_textures(bsp, lumps[LUMPS_NUMBERS['Textures']]),
-                parse_textures(bsp, lumps[LUMPS_NUMBERS['Effects']])) # effects have the same format as textures
+    try:
+        with open(bsp_path, 'rb') as bsp:
+            check_bsp_header(bsp)
+            lumps = parse_dir(bsp)
+            return (parse_entities(bsp, lumps[LUMPS_NUMBERS['Entities']]),
+                    parse_textures(bsp, lumps[LUMPS_NUMBERS['Textures']]),
+                    parse_textures(bsp, lumps[LUMPS_NUMBERS['Effects']])) # effects have the same format as textures
+    except IOError:
+        print 'Failed to open file', bsp_path        
 
-def check_header(bsp):
+def check_bsp_header(bsp):
     if bsp.read(4) != 'IBSP':
         raise Exception('Not bsp file')
 
 def parse_dir(bsp):
     bsp.seek(8)
     lumps = []
-    for i in range(0,16):
+    for i in range(0,17):
         lumps.append(struct.unpack('ii', bsp.read(8)))
     return lumps # lumps are in format (offset, length)
 
@@ -49,7 +53,11 @@ def parse_entities(bsp, lump):
         entity = entity_str.split('"')
         return entity[entity.index('classname') + 2]
 
-    entities = set(map(get_class_name, entities))
+    entities = list(set(map(get_class_name, entities)))
+
+    entities = filter((lambda e: e.find('weapon') != -1 or e.find('ammo') != -1
+                       or e.find('item') != -1 or e.find('misc_model') != -1),
+                      entities)
     return entities
 
 def parse_textures(bsp, lump):
@@ -61,3 +69,108 @@ def parse_textures(bsp, lump):
         bsp.read(8) # skip flags
     return textures
 
+import os
+
+# Returns list of files (script file, textures) needed by shaders with given names
+def get_files_for_shaders(shaders, baseoa):
+    scripts = os.listdir(baseoa + '/scripts')
+    textures_dep = []
+    scripts_dep = []
+    shaders_found = set()
+    
+    for script_path in scripts:
+        shaders_dep = parse_shader_file(baseoa + '/scripts/' + script_path)
+        for shader in shaders:
+            if shader in shaders_dep:
+#                print 'Shader', shader, 'found in script', script_path
+                shaders_found.add(shader)
+                if shaders_dep[shader] != []:
+                    scripts_dep.append(script_path)
+                    textures_dep.extend(shaders_dep[shader])
+                    
+    not_found = set(shaders).difference(shaders_found)    
+    for s in not_found:
+        print 'Warning: Shader', s, 'not found in any script.'
+        
+    return (list(set(scripts_dep)), list(set(textures_dep)), list(set(not_found))) #unique
+
+def parse_shader_file(script_path):
+    shaders = {}
+
+    with open(script_path, 'r') as script:
+        line = script.readline()
+        shader_name = ''
+        textures = []
+        state = 'global'
+        while line != '':
+            line = line.strip()
+            if line[:2] == '//' or line == '\n':
+                pass
+            elif state == 'global':
+                if line == '{':
+                    state = 'shader'
+                else:
+                    shader_name = line
+            elif state == 'shader':
+                if line == '{':
+                    state = 'stage'
+                elif line == '}':
+                    shaders[shader_name] = textures
+                    textures = []
+                    state = 'global'
+            elif state == 'stage':
+                if line[:3] == 'map' and line.find('$lightmap') == -1:
+                    textures.append(line[4:])
+                if line == '}':
+                    state = 'shader'
+
+            line = script.readline()
+            
+    return shaders
+
+
+# md3 dependencies (scripts and textures)
+def get_md3_deps(md3_path):
+    try:
+        with open(md3_path, 'rb') as md3:
+            check_md3_header(md3)
+            md3.seek(76) # seek to numbers and offsets
+            header = struct.unpack('iiiiiii', md3.read(28))
+            surface_num = header[2]
+            surface_offset = header[6]
+            print 'In header', surface_num, surface_offset
+            shaders = []
+            md3.seek(surface_offset)
+            for i in range(0, surface_num):
+                shaders.extend(check_md3_surface(md3))
+    except IOError:
+        print 'Failed to open file', md3_path
+
+    return shaders            
+        
+        
+def check_md3_header(md3):
+    md3.seek(0)
+    if md3.read(4) != 'IDP3':
+        raise Exception('Not md3 file')
+
+def check_md3_surface(md3):
+    start_offset = md3.tell()
+#    print 'In surface', s_offset
+    md3.seek(72, os.SEEK_CUR) # seek to numbers and offsets
+    surface_header = struct.unpack('iiiiiiiii', md3.read(36))
+    shaders_num = surface_header[1]
+    shaders_offset = surface_header[5]
+    
+    print 'In surface', shaders_num, shaders_offset
+    
+    md3.seek(start_offset + shaders_offset)
+    shaders = []
+    for i in range(0, shaders_num):
+        shaders.append((md3.read(64).strip('\x00'), struct.unpack('i', md3.read(4))[0]))
+
+    md3.seek(start_offset + surface_header[8]) # seek to the end of the surface
+    return shaders
+
+    
+    
