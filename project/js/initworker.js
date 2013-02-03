bb48a3004c4d8fab5cd1484cdc665346376e139e
Adam Rzepka <adam@desktop>
2013-02-03 09:32:03 +0100

    proxy calls work in compiled version

---
+++ project/js/initworker.js
@@ -0,0 +1,10 @@
+if (typeof COMPILED == 'undefined') {
+  var CLOSURE_BASE_PATH = '../../closure/goog/';
+  importScripts(
+      CLOSURE_BASE_PATH + 'bootstrap/webworkers.js',
+      CLOSURE_BASE_PATH + 'base.js',
+      CLOSURE_BASE_PATH + 'deps.js',
+      '../../deps.js');
+}
+
+importScripts('game/game.js');
