d76359837344a9ff98cff67b572868e573248e74
Adam Rzepka <adam@desktop>
2013-11-01 11:17:06 +0100

    fixed paths in initworker.js

--- project/js/initworker.js
+++ project/js/initworker.js
@@ -1,11 +1,11 @@
 // bootstrap webworker with gamecode in debug mode
 if (typeof COMPILED == 'undefined') {
-  var CLOSURE_BASE_PATH = '../../closure/goog/';
+  var CLOSURE_BASE_PATH = '../closure/goog/';
   importScripts(
       CLOSURE_BASE_PATH + 'bootstrap/webworkers.js',
       CLOSURE_BASE_PATH + 'base.js',
       CLOSURE_BASE_PATH + 'deps.js',
-      '../../deps.js');
+      '../deps.js');
 }
 goog.require('base.Broker');
 importScripts('game/game.js');
